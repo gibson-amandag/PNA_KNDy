9af707b9ade853d85faec5253f5b3f73662808bf
Amanda Gibson <gibsonag@umich.edu>
2021-06-24 15:26:47 -0400

    Update manuscript figure functions with manual geoms

--- Scripts/Functions/AG_KNDyPNA_GraphFunctions.R
+++ Scripts/Functions/AG_KNDyPNA_GraphFunctions.R
@@ -57,51 +57,28 @@ plotBurstParamBox <- function(
 }
 
 # param as character
-AG_KNDyPNA_manuscriptPlot <- function(df, param){
-  viz <- ggerrorplot(
-    df,
-    "GenTreatment",
-    param,
-    add = "jitter",
-    palette = c("white", "black"),
-    facet.by = "AgeGroup",
-    size = .4,
-    add.params = list(shape = 21, fill = "GenTreatment", size = 1, alpha = .8),
-    xlab = KNDy_VarNames[,"GenTreatment"],
-    ylab = KNDy_VarNames[, param],
-    error.plot = "linerange",
-    font.x = c(size = 11, style = "bold"),
-    font.y = c(size = 11, style = "bold"),
-    # font.family = "Arial",
-    font.tickslab = c(size = 11 ),
-    font.label = c(size = 11),
-    panel.labs.font = list(size = 11, face = "bold")
-  ) +
-    stat_summary(
-      geom = "errorbar", 
-      fun.min = mean, 
-      fun = mean, 
-      fun.max = mean, 
-      width = .7,
-      size = .4
+AG_KNDyPNA_manuscriptPlot <- function(df, param, strip.position = "bottom", niceNames = KNDy_VarNames){
+  viz <- df %>%
+    ggplot(
+      aes(x = GenTreatment, y = {{param}}, fill = GenTreatment)
     ) +
-    stat_summary(
-      geom = "errorbar", 
-      fun.min = median, 
-      fun = median, 
-      fun.max = median, 
-      width = .7,
-      size = .4,
-      color = "red",
-      alpha = .7
+    facet_wrap(
+      ~ AgeGroup,
+      strip.position = strip.position
     ) +
+    labs(y = niceNames %>% select({{ param }}))+
+    scale_fill_manual(values = c("white", "black"))+
+    addMeanHorizontalBar(width = 0.85, size = 0.4) +
+    addMeanSE_vertBar(size = 0.4)+
+    addMedianHorizontalBar(width = 0.85, size = 0.4)+
+    AG_KNDyPNA_jitterGeom()+
     expand_limits(y = 0) +
+    theme_pubr()+
     rremove(
       "legend"
     ) +
-    theme(
-      text = element_text(family = "Arial")
-    ) +
+    AG_KNDyPNA_textTheme()+
+    AG_KNDyPNA_boxTheme()+
     rremove("xlab")
   
   return(viz)
@@ -114,62 +91,34 @@ AG_KNDyPNA_manuscriptPlot_AGD <- function(
   addSig = TRUE,
   yAdd = 0.5 # for bumping up significance
 ){
-  viz <- ggerrorplot(
-    df,
-    "GenTreatment",
-    param,
-    add = "jitter",
-    palette = c("white", "black"),
-    size = .4,
-    add.params = list(shape = 21, fill = "GenTreatment", size = 1, alpha = .8),
-    xlab = KNDy_VarNames[,"GenTreatment"],
-    ylab = ylab,
-    error.plot = "linerange",
-    font.x = c(size = 11, style = "bold"),
-    font.y = c(size = 11, style = "bold"),
-    font.tickslab = c(size = 11 ),
-    font.label = c(size = 11),
-    panel.labs.font = list(size = 11, face = "bold")
-  ) +
-    stat_summary(
-      geom = "errorbar", 
-      fun.min = mean, 
-      fun = mean, 
-      fun.max = mean, 
-      width = .7,
-      size = .4
+  viz <- df %>%
+    ggplot(
+      aes(x = GenTreatment, y = {{param}}, fill = GenTreatment)
     ) +
-    expand_limits(y=0) +
+    labs(y = ylab)+
+    scale_fill_manual(values = c("white", "black"))+
+    addMeanHorizontalBar(width = 0.85, size = 0.4) +
+    addMeanSE_vertBar(size = 0.4)+
+    # addMedianHorizontalBar(width = 0.85, size = 0.4)+
+    AG_KNDyPNA_jitterGeom(width = 0.4)+
+    expand_limits(y = 0) +
+    theme_pubr()+
     rremove(
       "legend"
-    ) + 
-    rremove(
-      "xlab"
-    )
+    ) +
+    AG_KNDyPNA_textTheme()+
+    AG_KNDyPNA_boxTheme()+
+    rremove("xlab")
   
   if(addSig){
     viz <- viz +
       stat_compare_means(
         method = "t.test", 
-        label.y = max(df[, param], na.rm = TRUE) + yAdd,
+        label.y = max(df %>% select({{param}}), na.rm = TRUE) + yAdd,
         label = "p.signif", 
         ref.group = "CON"
       )
   }
-  
-  viz <- viz + theme(
-    # panel.background = element_rect(fill = "transparent"), # bg of the panel
-    panel.background = element_blank(), # bg of the panel
-    # plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
-    plot.background = element_blank(), # bg of the plot
-    panel.grid.major = element_blank(), # get rid of major grid
-    panel.grid.minor = element_blank(), # get rid of minor grid
-    # legend.background = element_rect(fill = "transparent"), # get rid of legend bg
-    legend.background = element_blank(), # get rid of legend bg
-    # legend.box.background = element_rect(fill = "transparent"), # get rid of legend panel bg
-    legend.box.background = element_blank(), # get rid of legend panel bg
-    text = element_text(family = "Arial")
-  )
   
   return(viz)
 }
@@ -190,20 +139,9 @@ AG_KNDyPNA_baseSenkPlot <- function(
       aes(group = CellID),
       position = position_dodge(0.4)
     ) +
-    geom_point(shape = 21, alpha = 0.8, aes(fill=GenTreatment,group=CellID), position = position_dodge(0.4), size = 1) +
-    stat_summary(
-      geom = "errorbar", 
-      fun.min = mean, 
-      fun = mean, 
-      fun.max = mean, 
-      width = .7,
-      size = .4
-    ) +
-    stat_summary(
-      geom = "linerange",
-      fun.data = mean_se,
-      size = 0.4
-    ) +
+    geom_point(shape = 21, alpha = 1, aes(fill=GenTreatment,group=CellID), position = position_dodge(0.4), size = 1.2) +
+    addMeanHorizontalBar(width = 0.85)+
+    addMeanSE_vertBar()+
     scale_fill_manual(values = c("white", "black")) +
     theme_pubr() +
     rremove("xlab") +
@@ -224,20 +162,8 @@ AG_KNDyPNA_gridSenktidePlot <- function(
     rremove(
       "legend"
     ) +
-    theme(
-      strip.text = element_text(
-        size = 11, 
-        family = "Arial",
-        face = "bold"),
-      axis.text = element_text(
-        size = 11,
-        family = "Arial"
-      ),
-      axis.title = element_text(
-        size = 11, 
-        family = "Arial",
-        face = "bold")
-    )
+    AG_KNDyPNA_textTheme()+
+    AG_KNDyPNA_boxTheme()
   return(gridPlot)
 }
 
@@ -248,61 +174,51 @@ AG_KNDyPNA_longSenktidePlot <- function(
     facet_wrap(
       ~ TreatxAge,
       # ~ AgeGroup + GenTreatment,
-      # strip.position = "bottom",
+      strip.position = "bottom",
       ncol = 4,
       nrow = 1
     )+
     rremove(
       "legend"
     ) + 
-    theme(
-      # strip.background = element_blank(),
-      # strip.placement = "outside",
-      strip.text = element_text(size = 11, family = "Arial", face = "bold"),
-      axis.text = element_text(size = 11, family = "Arial"),
-      axis.title = element_text(size = 11, family = "Arial", face = "bold")
-    )
+    AG_KNDyPNA_textTheme()+
+    AG_KNDyPNA_boxTheme()
   return(longPlot)
 }
 
 AG_KNDyPNA_plotCyclesPercent <- function(
   df,
   ylabel = "% days in stage",
-  alpha = 0.8,
-  dotSize = 1,
+  alpha = 1,
+  dotSize = 1.2,
   barWidth = 0.7,
   barSize = 0.4,
   addMedian = FALSE,
   medianColor = "red",
   medianAlpha = 0.7,
-  strip.position = "top"
+  strip.position = "bottom"
 ){
-  viz <- ggerrorplot(
-    df,
-    x = "GenTreatment",
-    y = "percent",
-    palette = c("white", "black"),
-    add = "jitter", 
-    facet.by = "stage",
-    add.params = AG_KNDyPNA_jitterParams(size = dotSize, alpha = alpha),
-    xlab = "",
-    ylab = ylabel,
-    error.plot = "linerange",
-    strip.position = strip.position
-  ) +
+  viz <- df %>%
+    ggplot(
+      aes(x = GenTreatment, y = percent, fill = GenTreatment)
+    ) +
+    facet_wrap(
+      ~ stage,
+      strip.position = strip.position
+    ) +
+    labs(y = ylabel)+
+    scale_fill_manual(values = c("white", "black"))+
+    addMeanHorizontalBar(width = 0.85, size = 0.4) +
+    addMeanSE_vertBar(size = 0.4)+
+    AG_KNDyPNA_jitterGeom()+
     expand_limits(y = 0) +
-    rremove("xlab") +
-    rremove("legend") + 
-    addMeanHorizontalBar(width = barWidth, size = barSize)
-  
-  if(strip.position == "bottom"){
-    viz <- viz + theme(
-      strip.placement = "outside",
-      strip.background = element_blank()
-    )
-  }
-  
-  viz <- viz + AG_KNDyPNA_textTheme()
+    theme_pubr()+
+    rremove(
+      "legend"
+    ) +
+    AG_KNDyPNA_textTheme()+
+    AG_KNDyPNA_boxTheme()+
+    rremove("xlab")
     
   
   if(addMedian){
@@ -318,8 +234,8 @@ AG_KNDyPNA_plotCyclesPercent <- function(
 }
 
 AG_KNDyPNA_jitterParams <- function(
-  size = 1,
-  alpha = 0.8
+  size = 1.2,
+  alpha = 1
 ){
   params <- list(
     shape = 21,
@@ -330,6 +246,21 @@ AG_KNDyPNA_jitterParams <- function(
   return(params)
 }
 
+AG_KNDyPNA_jitterGeom <- function(
+  size = 1.2,
+  alpha = 1,
+  width = 0.35,
+  height = 0
+){
+  geom_jitter(
+    shape = 21,
+    size = size,
+    alpha = alpha,
+    width = width,
+    height = height
+  ) 
+}
+
 AG_KNDyPNA_textTheme <- function(size = 11){
   theme = theme(
     text = element_text(size = size, family = "Arial"),
@@ -337,6 +268,15 @@ AG_KNDyPNA_textTheme <- function(size = 11){
     axis.title = element_text(face = "bold")
   )
   return(theme)  
+}
+
+AG_KNDyPNA_boxTheme <- function(axisSize = 0.5){
+  theme = theme(
+    panel.border = element_blank(),
+    axis.line = element_line(color = "black", size = axisSize),
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
 }
 
 addMeanHorizontalBar <- function(
@@ -349,6 +289,16 @@ addMeanHorizontalBar <- function(
     fun = mean, 
     fun.max = mean, 
     width = width,
+    size = size
+  )
+}
+
+addMeanSE_vertBar <- function(
+  size = 0.4
+){
+  stat_summary(
+    geom = "linerange", 
+    fun.data = mean_se,
     size = size
   )
 }
@@ -403,7 +353,8 @@ AG_KNDyPNA_plotCycleTraces <- function(
   
   viz <- viz + 
     theme_pubr() +
-    AG_KNDyPNA_textTheme()
+    AG_KNDyPNA_textTheme() + 
+    AG_KNDyPNA_boxTheme()
   
   if(!is.null(enquo(lineColorVar))){
     viz <- viz + scale_color_manual(values = colorKey)
@@ -420,6 +371,22 @@ AG_KNDyPNA_plotCycleTraces <- function(
     )
   }
   
-  
+  return(viz)
+}
+
+AG_KNDyPNA_plotPropFiring <- function(df) {
+  viz <- ggplot(df, aes(x = GenTreatment, fill = firing))+
+    geom_bar(position = "fill", color = "black") +
+    geom_text(aes(label = ..count..), stat = "count", vjust = 1.3, colour = "darkgrey", position = "fill")+
+    labs(y = "proportion firing") + 
+    scale_fill_manual(values = c("white", "black")) +
+    facet_wrap("AgeGroup",
+               strip.position = "bottom"
+    ) +
+    theme_pubr(base_size = 11, base_family = "Arial") +
+    AG_KNDyPNA_textTheme()+
+    AG_KNDyPNA_boxTheme()+
+    rremove("legend") +
+    rremove("xlab")
   return(viz)
 }
